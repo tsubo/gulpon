752a96d49beef3320d2c3d93de1c6ba09fdb0dce
tsubo <ktsubota@gmail.com>
2018-10-13 08:25:50 +0900

    fix error handling for gulp webpack task

--- gulp/tasks/webpack.js
+++ gulp/tasks/webpack.js
@@ -3,10 +3,10 @@ const gulp = require('gulp')
 const webpack = require('webpack')
 const webpackStream = require('webpack-stream')
 const webpackConfig = require('../../webpack.config')
+const util = require('../util')
 
-// FIXME: コンパイルがエラーのとき、サーバーが止まってしまう
 gulp.task('webpack', () => {
-  return webpackStream(webpackConfig, webpack).pipe(
-    gulp.dest(`${config.path.destDir}/js`),
-  )
+  return webpackStream(webpackConfig, webpack)
+    .on('error', util.swallowError)
+    .pipe(gulp.dest(`${config.path.destDir}/js`))
 })
