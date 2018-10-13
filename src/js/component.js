3f811ead6edb755b2c39552d9709a2967c2b102f
tsubo <ktsubota@gmail.com>
2018-10-14 06:39:49 +0900

    simplify component.js

--- src/js/component.js
+++ src/js/component.js
@@ -3,20 +3,4 @@
  */
 import Example from './component/Example.vue'
 
-export function install(Vue) {
-  Vue.component('example-component', Example)
-}
-
-const plugin = {
-  install,
-}
-
-let GlobalVue = null
-if (typeof window !== 'undefined') {
-  GlobalVue = window.Vue
-} else if (typeof global !== 'undefined') {
-  GlobalVue = global.Vue
-}
-if (GlobalVue) {
-  GlobalVue.use(plugin)
-}
+window.Vue.component('example-component', Example)
